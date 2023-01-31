70acc9b667b9a08015f62c54c2b76e3bb65dee19
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-01-31 15:06:03 -0600

    new apis works

--- src/Components/AxiosAPIS/AxiosApis.js
+++ src/Components/AxiosAPIS/AxiosApis.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import React from 'react'
 
-const baseUrl = "https://jsonplaceholder.typicode.com/users"
+// const baseUrl = "https://jsonplaceholder.typicode.com/users"
+const baseUrl = "https://dummyjson.com/users"
 
 export default function Api() {
     const [get, setGet] = React.useState(null);
@@ -16,9 +17,13 @@ export default function Api() {
   
     return (
       <div>
-        <h1>{get[0].email}</h1>
+        {/* <h1>{get[0].email}</h1>
         <p>{get[0].name}</p>
-        <h3>{[0].password}</h3>
+        <h3>{[0].password}</h3> */}
+
+        <h1>{get.users[0].email}</h1>
+        <p>{get.users[0].name}</p>
+        <h3>{get.users[0].password}</h3>
       </div>
     );
   }
